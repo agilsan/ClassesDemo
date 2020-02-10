33187478e382fc058f842319ea50554369e60c9e
agil <antonio_g_s@hotmail.com>
2020-02-10 23:16:25 +0100

    Initial Commit

---
+++ ClassesDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ClassesDemo
+//
+//  Created by Antonio Gil Sánchez on 10/02/2020.
+//  Copyright © 2020 Antonio Gil Sánchez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
